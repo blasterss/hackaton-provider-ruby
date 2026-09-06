07317637480ead52a4503811a199fbe8c1f3dd91
blasterss <anatolijtrubnikov@yandex.ru>
2026-09-06 16:45:53 +0300

    feat: Добавлено явное определение сервера для BaseURL

--- src/lib/provider_integrator/extractors/provider_extractor.rb
+++ src/lib/provider_integrator/extractors/provider_extractor.rb
@@ -11,7 +11,7 @@ module ProviderIntegrator
           name: provider_name,
           slug: technical_name,
           class_name: class_name,
-          base_url: document.servers.first&.url,
+          base_url: production_server&.url,
           base_url_env_name: "#{technical_name.upcase}_BASE_URL"
         )
       end
@@ -33,6 +33,19 @@ module ProviderIntegrator
 
           value
         end
+      end
+
+      def production_server
+        servers = document.servers.to_a
+        described_server = servers.find do |server|
+          server.description.to_s.match?(/\bproduction\b|\blive\b/i)
+        end
+        return described_server if described_server
+
+        non_sandbox_server = servers.find do |server|
+          server.url !~ /sandbox|staging|test/i
+        end
+        non_sandbox_server || servers.first
       end
     end
   end
